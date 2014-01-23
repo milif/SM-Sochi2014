61576df580964b9c53dcf765b7b7b8208c84895b
Alexander <zidiganov@gmail.com>
2014-01-24 00:49:04 +0600

    feat(stmClimber): создание IndexPage

---
+++ src/stmIndexPage/directives/stmIndexPageScreen/stmIndexPageScreen.js
@@ -0,0 +1,44 @@
+/**
+ * @ngdoc directive
+ * @name stmIndexPage.directive:stmIndexPageScreen
+ * @function
+ *
+ * @requires stmIndexPage.directive:stmIndexPageScreen:b-IndexPage.css
+ * @requires stmIndexPage.directive:stmIndexPageScreen:template.html
+ *
+ * @description
+ * Главная страница
+ *
+ * @element ANY
+ *
+ * @example
+    <example module="appExample">
+      <file name="index.html">
+         <div stm-index-page class="example-screen"></div>
+      </file>
+      <file name="style.css">
+         .in-plunkr, .in-plunkr body, .in-plunkr .well {
+            height: 100%;
+            margin: 0;
+         }
+         .doc-example-live .example-screen {
+            height: 500px;
+            }
+         .example-screen {
+            width: 100%;
+            height: 100%;
+            position: relative;
+            overflow: hidden;
+            }
+      </file>
+    </example>
+    
+ */
+
+angular.module('stmIndexPage').directive('stmIndexPageScreen', function(){  
+    return {
+        transclude: true,
+        templateUrl: 'partials/stmIndexPage.directive:stmIndexPageScreen:template.html'
+    };
+});
+
