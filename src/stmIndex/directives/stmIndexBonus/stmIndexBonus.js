65aeedb34a13615e730ec250ade149e7a67e4fa9
Your Name <milif777@gmail.com>
2014-01-30 21:02:22 +0400

    feat(stmIndexBonus): значки бонусов

--- src/stmIndex/directives/stmIndexBonus/stmIndexBonus.js
+++ src/stmIndex/directives/stmIndexBonus/stmIndexBonus.js
@@ -10,14 +10,16 @@
  * Bonus
  *
  * @element ANY
+ * @param {String} stmIndexBonus component id 
+ * @param {Array} position set bonus position [x, y]
+ * @param {String} type bonus type (sber|mnogo|pickpoint)
  *
  * @example
     <example module="appExample">
       <file name="index.html">
-        <div class="b-sample">
-          <div stm-index-bonus="bonusId1" type="pickpoint" position="[0,0]"></div>
-          <div stm-index-bonus="bonusId2" type="mnogo" position="[100,0]"></div>
-          <div stm-index-bonus="bonusId3" type="sber" position="[200,0]"></div>
+        <div class="b-sample" ng-controller="bonusesCtrl">
+          <div ng-repeat="bonus in bonuses" stm-index-bonus="bonusId{{$index}}" type="{{bonus.type}}" position="{{bonus.pos}}"></div>
+          <div stm-index-bonus="bonusId3" type="sber" position="[350, 50]"></div>
         <div>
       </file>
       <file name="style.css">
@@ -25,29 +27,55 @@
           position:relative;
           z-index:1;
           height: 200px;
-          width: 200px;
+          width: 400px;
       }
+      </file>
+      <file name="controller.js">
+        function bonusesCtrl($scope, $timeout){
+            $scope.bonuses = [
+                {pos: [50, 50], type: "mnogo" }, 
+                {pos: [150, 50], type: "sber"}, 
+                {pos: [250, 50], type: "pickpoint"}
+            ];
+            for(var i=0;i<$scope.bonuses.length;i++){
+                $timeout(function(){
+                    i--
+                    $scope.$broadcast('removeBonus-bonusId' + i);
+                }, 1000 * (i+1));
+            }
+        }
       </file>
     </example>
     
  */
-
+/**
+   * @ngdoc event
+   * @name stmIndex.directive:stmIndexBonus#removeBonus-ID
+   * @eventOf stmIndex.directive:stmIndexBonus
+   * @eventType local on scope
+   * @description
+   * Инициирует скрытие компонента 
+   * 
+   */
 angular.module('stmIndex').directive('stmIndexBonus', function(){
     return {
         scope: {
-            'stm-index-bonus': '@',
+            stmIndexBonus: '@',
             type: '@',
-            position: '='
+            position: '@'
         },
         transclude: true,
         templateUrl: 'partials/stmIndex.directive:stmIndexBonus:template.html',
-        controller: ['$scope', '$element', '$attrs', '$animate', function($scope, $element, $attrs, $animate){
-            $element.find('>div').css({
-                left: $scope.position[0] + 'px',
-                top: $scope.position[1] + 'px'
+        controller: ['$scope', '$animate', function($scope, $animate){
+            $scope.$watch('position', function(){
+                var position = $scope.$eval($scope.position);
+                $scope.css = {
+                    left: position[0],
+                    top: position[1]
+                }
             });
-            $scope.$on('removeBonus-'+$scope['stm-index-bonus'], function(event, e){
-                $animate.leave($element);
+            $scope.$on('removeBonus-' + $scope.stmIndexBonus, function(){
+                $scope.hide = true;
             });
         }]
     };
