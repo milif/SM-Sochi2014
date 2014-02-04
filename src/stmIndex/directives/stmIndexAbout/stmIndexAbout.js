a4596a087b9f092ec50d824ebc514053cbd092d8
Alexander <zidiganov@gmail.com>
2014-02-04 20:59:13 +0600

    feat(stmIndexAbout): верстка странички (2.33)

---
+++ src/stmIndex/directives/stmIndexAbout/stmIndexAbout.js
@@ -0,0 +1,36 @@
+/**
+ * @ngdoc directive
+ * @name stmIndex.directive:stmIndexAbout
+ * @function
+ *
+ * @requires stmIndex.directive:stmIndexAbout:b-about.css
+ * @requires stmIndex.directive:stmIndexAbout:template.html
+ *
+ * @description
+ * About
+ *
+ * @element ANY
+ *
+ * @example
+    <example module="appExample">
+      <file name="index.html">
+        <div stm-index-about></div>
+      </file>
+      <file name="style.css">
+      .b-sample {
+          position:relative;
+          z-index:1;
+          height: 200px;
+          width: 400px;
+      }
+      </file>
+    </example>
+    
+ */
+
+
+angular.module('stmIndex').directive('stmIndexAbout', function(){
+    return {
+        templateUrl: 'partials/stmIndex.directive:stmIndexAbout:template.html'
+    };
+});
