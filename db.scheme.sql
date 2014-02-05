1e6976290b4420b96af8c19b8219252dfc511fcc
Your Name <milif777@gmail.com>
2014-02-06 02:46:51 +0400

    feat(ServerSide): авторизация, социалка и игры

--- db.scheme.sql
+++ db.scheme.sql
@@ -3,17 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Фев 06 2014 г., 02:25
+-- Время создания: Фев 06 2014 г., 02:45
 -- Версия сервера: 5.5.35
 -- Версия PHP: 5.5.3-1ubuntu2.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- База данных: `sochi2014`
@@ -31,9 +25,10 @@ CREATE TABLE IF NOT EXISTS `game_log` (
   `type` varchar(32) NOT NULL,
   `action` varchar(32) NOT NULL,
   `data` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -62,8 +57,10 @@ CREATE TABLE IF NOT EXISTS `social_log` (
   `ip` varchar(32) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `type` (`type`,`uid`,`time`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  KEY `uid` (`uid`),
+  KEY `type` (`type`),
+  KEY `time` (`time`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -78,3 +75,4 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`),
   KEY `uri` (`uri`(333))
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
