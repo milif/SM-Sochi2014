e259644990c903ec5cf486f539173adbdefb398f
Your Name <milif777@gmail.com>
2014-02-07 13:25:43 +0400

    fix(stmGameEti): стрельба по панели

--- src/stmGameEti/directives/stmGameEtiScreen/stmGameEtiScreen.js
+++ src/stmGameEti/directives/stmGameEtiScreen/stmGameEtiScreen.js
@@ -123,7 +123,7 @@ angular.module('stmGameEti').directive('stmGameEtiScreen', ['$compile', '$rootSc
                     e.preventDefault();
                 },
                 'click': function(e){
-                    if($(e.target).closest('[data-game]').length == 0) return;
+                    if($(e.target).closest(viewEl).length == 0) return;
                     photo(e);
                 }          
             }
