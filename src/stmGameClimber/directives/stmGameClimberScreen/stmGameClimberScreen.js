5a9962552d89d2b168c52640c20303cfee3bb688
Vitalii Rybalka <ext-vrybalka@netbiscuits.com>
2014-02-21 15:27:59 +0200

    feat(stmGameClimber): fix achieve amateurfauna

--- src/stmGameClimber/directives/stmGameClimberScreen/stmGameClimberScreen.js
+++ src/stmGameClimber/directives/stmGameClimberScreen/stmGameClimberScreen.js
@@ -148,8 +148,8 @@ angular.module('stmGameClimber').directive('stmGameClimberScreen',['$timeout', '
                         missedBonusTime = null,
                         positionChangesNumber = 0,
 
-                        positionOvis1 = 3700,
-                        positionOvis2 = 7800,
+                        positionOvis1 = 4600,
+                        positionOvis2 = 7600,
                         ovisesPassedCounts = 0,
                         ovisesIsInside = false,
 
