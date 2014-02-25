e7dfdaee05e0d2e01ca51a3f78b7bd0747240702
Your Name <milif777@gmail.com>
2014-02-26 01:36:28 +0400

    fix(subscribe): partner ref

--- app/lib/Subscribe.class.php
+++ app/lib/Subscribe.class.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/DB.class.php';
 
 class Subscribe {
     static public function add($email){ 
-        DB::query("INSERT INTO subscribe (email) VALUES (:email);", array(":email" => $email));
+        DB::query("INSERT INTO subscribe (email, partner_ref, partner_subref) VALUES (:email, :partnerRef, :partnerSubref);", array(":email" => $email, ':partnerRef'=>$_COOKIE['partner'], ':partnerSubref'=>$_COOKIE['subref']));
         return true;
     }
 }
