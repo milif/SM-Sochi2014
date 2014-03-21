a4feb2ffab3356ceecb11ee1be99291accad133e
Alexander <zidiganov@gmail.com>
2014-03-21 15:15:07 +0600

    feat(stmIndexSochiPrice): верстка странички

---
+++ src/stmIndex/directives/stmIndexSochiPrice/stmIndexSochiPrice.js
@@ -0,0 +1,39 @@
+"use strict";
+/**
+ * @ngdoc directive
+ * @name stmIndex.directive:stmIndexSochiPrice
+ * @function
+ *
+ * @requires stmIndex.directive:stmIndexSochiPrice:b-sochiprice.css
+ * @requires stmIndex.directive:stmIndexSochiPrice:template.html
+ *
+ * @requires stmIndex.directive:stmIndexToolbar
+ *
+ * @description
+ * About
+ *
+ * @element ANY
+ *
+ * @example
+    <example module="appExample">
+      <file name="index.html">
+        <div class="b-sample">
+            <div stm-index-sochi-price></div>
+        </div>
+      </file>
+      <file name="style.css">
+      .b-sample {
+          height: 600px;
+          overflow: auto;
+      }
+      </file>
+    </example>
+    
+ */
+
+
+angular.module('stmIndex').directive('stmIndexSochiPrice', function(){
+    return {
+        templateUrl: 'partials/stmIndex.directive:stmIndexSochiPrice:template.html'
+    };
+});
