8595626c4eddf4936e1b893ac8244725bc6ae4a5
Your Name <milif777@gmail.com>
2014-03-24 10:36:01 +0400

    feat(stmIndexPrice): внедрение

---
+++ src/stmIndex/resources/stmGoods.js
@@ -0,0 +1,36 @@
+"use strict";
+    /**
+     * @ngdoc interface
+     * @name stmIndex.stmGoods
+     * @description
+     *
+     * Внешний интерфейс товаров
+     * 
+     */    
+    /**
+       * @ngdoc method
+       * @name stmIndex.stmGoods#getItems
+       * @methodOf stmIndex.stmGoods
+       *
+       * @description
+       * Выборка товаров
+       *            
+       */         
+    angular.module('stmIndex').factory('stmGoods', ['$resource', function($resource){
+    
+        var stmGoods = $resource('api/goods.php?c=:category&o=:offset&l=:limit&s=:order');
+        
+        stmGoods.getItems = getItems;
+        
+        return stmGoods;
+        
+        function getItems(category, offset, limit, order){
+            return stmGoods.get({
+                category: category, 
+                offset: offset, 
+                limit: limit, 
+                order: order
+            });
+        };
+        
+    }]) 
