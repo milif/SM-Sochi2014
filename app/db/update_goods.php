4e3053af3a2aa0155e06396c58f1142c1fbe32b4
Your Name <milif777@gmail.com>
2014-04-07 21:28:08 +0400

    feat(app): распродажа квелли и проскайтер

--- app/db/update_goods.php
+++ app/db/update_goods.php
@@ -19,6 +19,7 @@ function updateSotmarket(){
     $doc = new DOMDocument();
     $doc->load($STMXML);
     $items = $doc->getElementsByTagName ('category');
+    
     foreach($items as $itemNode){
         $attrsNodeL = $itemNode->attributes;
         $urlNode = $attrsNodeL->getNamedItem('url');
@@ -70,12 +71,14 @@ function updateProskater(){
         );
     }
     $items = $doc->getElementsByTagName ('offer');
+    $count = 0;
     foreach($items as $itemNode){
+        if($count++ == 500) break;
         $available = $itemNode->attributes->getNamedItem('available')->textContent == 'true';
         //$saled = $itemNode->attributes->getNamedItem('saled')->textContent == 'true';
         if(!$available) continue;
         $itemData = nodeToArray($itemNode);
-        if(!isset($itemData['oldprice'])) continue;
+        //if(!isset($itemData['oldprice'])) continue;
         $params = array(
             ':url' => $itemData['url']['value'],
             ':title' =>	$itemData['name']['value'],
@@ -83,9 +86,9 @@ function updateProskater(){
             ':subName' => $categories[$itemData['categoryId']['value']]['title'],
             ':subUrl' => $categories[$itemData['categoryId']['value']]['url'],
             ':price' => (int)$itemData['price']['value'],
-            ':oldprice' => (int)$itemData['oldprice']['value'],
+            ':oldprice' => isset($itemData['oldprice']) ? (int)$itemData['oldprice']['value'] : null,
             ':category' => 'sport',
-            ':discount' => (1 - (int)$itemData['price']['value'] / (int)$itemData['oldprice']['value']) * 100,
+            ':discount' => isset($itemData['oldprice']) ? (1 - (int)$itemData['price']['value'] / (int)$itemData['oldprice']['value']) * 100 : 0,
             ':ratio' =>	(int)$itemData['picture']['attrs']['width'] / (int)$itemData['picture']['attrs']['height'],
             ':saled' => 0
         );
@@ -94,8 +97,8 @@ function updateProskater(){
 }
 function updateQuelle(){
     $XML = array(
-        //'http://www.quelle.ru/nws_exports/sotmarket/productfeed_sotmarket_quelle_ru_ru.xml',
-        'http://www.quelle.ru/nws_exports/sotmarket/productfeed_sotmarket_sale_quelle_ru_ru.xml'
+        'http://www.quelle.ru/nws_exports/sotmarket/productfeed_sotmarket_sale_quelle_ru_ru.xml',
+        'http://www.quelle.ru/nws_exports/sotmarket/productfeed_sotmarket_quelle_ru_ru.xml'        
     );
     
     foreach($XML as $_xml) _updateQuelle($_xml);
@@ -115,21 +118,24 @@ function _updateQuelle($STMXML){
         );
     }
     $items = $doc->getElementsByTagName ('offer');
+    $count = 0;
     foreach($items as $itemNode){
+        if($count++ == 500) break;
         $available = $itemNode->attributes->getNamedItem('available')->textContent == 'true';
         //$saled = $itemNode->attributes->getNamedItem('saled')->textContent == 'true';
         //if(!$available) continue;
         $itemData = nodeToArray($itemNode);
+        if(!isset($itemData['picture'])) continue;
         $params = array(
             ':url' => $itemData['url']['value'].(isset($itemData['promo']['value']) ? '?coupon='.$itemData['promo']['value'] : ''),
             ':title' =>	preg_replace('/[\s\.]+/', '', $itemData['model']['value']),
             ':img' => preg_replace('/_w\d+_h\d+/', '_w197_h205', $itemData['picture']['value']),
             ':subName' => $categories[$itemData['categoryId']['value']]['title'],
-            ':subUrl' => NULL,
+            ':subUrl' => '',
             ':price' => (int)$itemData['price']['value'],
             ':oldprice' => isset($itemData['oldprice']) ? (int)$itemData['oldprice']['value'] : NULL,
             ':category' => 'clothing',
-            ':discount' => isset($itemData['oldprice']) ? (1 - (int)$itemData['price']['value'] / (int)$itemData['oldprice']['value']) * 100 : NULL,
+            ':discount' => isset($itemData['oldprice']) ? (1 - (int)$itemData['price']['value'] / (int)$itemData['oldprice']['value']) * 100 : 0,
             ':ratio' =>	NULL,
             ':saled' => 0
         );
