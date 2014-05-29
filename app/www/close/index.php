36961cf3d07663e35029b5e3bb437cdfafa1508f
Михаил <milif777@gmail.com>
2014-05-29 22:05:57 +0400

    feat(close): доделки

--- app/www/close/index.php
+++ app/www/close/index.php
@@ -18,18 +18,12 @@
         <div class="splash">
             <a class="splash__sotmarket" href="/">Sotmarket.ru</a>
             <div class="splash__title"></div>
-
-            <div class="splash__text">
-                <p>Сочные игры это атомистика, по определению, индуктивно подчеркивает знак, ломая рамки привычных представлений. Гегельянство индуцирует данный дедуктивный метод, хотя в официозе принято обратное.</p>
-                <p>Гетерономная этика ментально дискредитирует язык образов, открывая новые горизонты. Вещь в себе трансформирует интеллигибельный катарсис, tertium nоn datur. Заблуждение, как принято считать, трогательно наивно.</p>
-            </div>
-
             <div class="splash__envelop">
                 <div class="splash__envelop-h">
                     <div ng-if="state == 'send'" class="splash__envelop-subscribe">
                         <div class="splash__envelop-mail"></div>
-                        <div class="splash__envelop-note">Хотите знать обо всех проектах Сотмаркета?</div>
-                        <div class="splash__envelop-title">Подпишитесь, и узнаете первыми</div>
+                        <div class="splash__envelop-note">Сочные игры закончились, а крутые подарки еще остались.<br>Будь в курсе новых акций и распродаж!</div>
+                        <div class="splash__envelop-title">Подпишись, и узнай первым</div>
 
                         <form name="model.form" class="splash__envelop-form g-form" ng-submit="submit()" novalidate>
                             <div class="splash__envelop-form-row">
